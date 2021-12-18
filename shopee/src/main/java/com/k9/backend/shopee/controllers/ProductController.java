d41db8c5b247b00eadb1bf7e65bbe1df262f2499
Krit Chomaitong <luckkrit@hotmail.com>
2021-12-18 23:47:49 +0700

    modify product controller

--- shopee/src/main/java/com/k9/backend/shopee/controllers/ProductController.java
+++ shopee/src/main/java/com/k9/backend/shopee/controllers/ProductController.java
@@ -1,13 +1,18 @@
 package com.k9.backend.shopee.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.k9.backend.shopee.dto.ProductDTO;
 import com.k9.backend.shopee.services.ProductService;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +24,18 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<ProductDTO>> getAllProducts() {
-        return ResponseEntity.ok(productService.getAllProducts());
+    public ResponseEntity<List<ProductDTO>> getAllProducts(@RequestParam Optional<Integer> limit,
+            @RequestParam Optional<String> sort) {
+        return ResponseEntity.ok(this.productService.getAllProducts(limit, sort));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getAllProducts(@PathVariable Long id) {
+        var optionalProductDTO = productService.getProduct(id);
+        if (optionalProductDTO.isPresent())
+            return ResponseEntity.ok(optionalProductDTO.get());
+        return ResponseEntity.notFound().build();
+
+    }
+
 }
--- shopee/src/main/java/com/k9/backend/shopee/services/ProductService.java
+++ shopee/src/main/java/com/k9/backend/shopee/services/ProductService.java
@@ -2,11 +2,15 @@ package com.k9.backend.shopee.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.k9.backend.shopee.dto.ProductDTO;
 import com.k9.backend.shopee.repository.ProductRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +20,67 @@ import lombok.RequiredArgsConstructor;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public List<ProductDTO> getAllProducts() {
-        return this.productRepository.findAll().stream()
+    public List<ProductDTO> getAllProducts(Optional<Integer> limit, Optional<String> sort) {
+        if (limit.isEmpty() && sort.isEmpty())
+            return this.productRepository.findAll().stream()
+                    .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                            product.getCategory().getTitle(), product.getDescription(), product.getImage()))
+                    .collect(Collectors.toList());
+        else if (limit.isPresent() && sort.isPresent()) {
+            if (sort.get().equals("asc")) {
+                return this.productRepository.findAll(PageRequest.of(0, limit.get(), Sort.by(Sort.Direction.ASC, "id")))
+                        .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                                product.getCategory().getTitle(), product.getDescription(), product.getImage()))
+                        .getContent();
+            } else {
+                return this.productRepository
+                        .findAll(PageRequest.of(0, limit.get(), Sort.by(Sort.Direction.DESC, "id")))
+                        .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                                product.getCategory().getTitle(), product.getDescription(), product.getImage()))
+                        .getContent();
+
+            }
+        } else if (limit.isPresent() && sort.isEmpty()) {
+            return this.productRepository.findAll(PageRequest.of(0, limit.get()))
+                    .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                            product.getCategory().getTitle(), product.getDescription(), product.getImage()))
+                    .getContent();
+
+        } else {
+            if (sort.get().equals("asc"))
+                return this.productRepository
+                        .findAll(
+                                PageRequest.of(0, (int) this.productRepository.count(),
+                                        Sort.by(Sort.Direction.ASC, "id")))
+                        .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                                product.getCategory().getTitle(), product.getDescription(), product.getImage()))
+                        .getContent();
+            else
+                return this.productRepository
+                        .findAll(
+                                PageRequest.of(0, (int) this.productRepository.count(),
+                                        Sort.by(Sort.Direction.DESC, "id")))
+                        .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                                product.getCategory().getTitle(), product.getDescription(), product.getImage()))
+                        .getContent();
+
+        }
+    }
+
+    public Optional<ProductDTO> getProduct(Long productId) {
+        var optionalProduct = this.productRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            var product = optionalProduct.get();
+            var productDTO = new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
+                    product.getCategory().getTitle(), product.getDescription(), product.getImage());
+            return Optional.of(productDTO);
+        } else
+            return Optional.empty();
+    }
+
+    public Page<ProductDTO> getProductsByLimit(int limit) {
+        return this.productRepository.findAll(PageRequest.of(0, limit, Sort.by(Sort.Direction.ASC, "id")))
                 .map(product -> new ProductDTO(product.getId(), product.getTitle(), product.getPrice(),
-                        product.getCategory().getTitle(), product.getDescription(), product.getImage()))
-                .collect(Collectors.toList());
+                        product.getCategory().getTitle(), product.getDescription(), product.getImage()));
     }
 }
