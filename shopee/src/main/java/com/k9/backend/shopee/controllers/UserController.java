58465e0fa7a35f74a36550e0f08c2494dac81612
Krit Chomaitong <luckkrit@hotmail.com>
2021-12-22 23:02:47 +0700

    add list user

--- shopee/src/main/java/com/k9/backend/shopee/controllers/UserController.java
+++ shopee/src/main/java/com/k9/backend/shopee/controllers/UserController.java
@@ -1,13 +1,21 @@
 package com.k9.backend.shopee.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.k9.backend.shopee.dtos.user.AddUserDTO;
+import com.k9.backend.shopee.dtos.user.UserDTO;
+import com.k9.backend.shopee.services.UserService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +25,23 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/users")
 public class UserController {
     Logger logger = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers(@RequestParam Optional<Integer> limit,
+            @RequestParam Optional<String> sort) {
+        return ResponseEntity.ok(this.userService.getAllUsers(limit, sort));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        var optionalUserDTO = this.userService.getUser(id);
+        if (optionalUserDTO.isPresent()) {
+            return ResponseEntity.ok(optionalUserDTO.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     @PostMapping
     public ResponseEntity<String> addUser(@RequestBody AddUserDTO addUserDTO) {
---
+++ shopee/src/main/java/com/k9/backend/shopee/services/UserService.java
@@ -0,0 +1,76 @@
+package com.k9.backend.shopee.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.k9.backend.shopee.dtos.user.AddressDTO;
+import com.k9.backend.shopee.dtos.user.GeolocationDTO;
+import com.k9.backend.shopee.dtos.user.NameDTO;
+import com.k9.backend.shopee.dtos.user.UserDTO;
+import com.k9.backend.shopee.models.User;
+import com.k9.backend.shopee.repository.UserRepository;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<UserDTO> getAllUsers(Optional<Integer> limit, Optional<String> sort) {
+        List<User> users = userRepository.findAll(this.getPageable(limit, sort)).getContent();
+        return users.stream().map(user -> this.getUserDTO(user)).collect(Collectors.toList());
+    }
+
+    public Optional<UserDTO> getUser(Long userId) {
+        var optionalUser = this.userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            return Optional.of(this.getUserDTO(optionalUser.get()));
+        } else {
+            return Optional.ofNullable(null);
+        }
+    }
+
+    private UserDTO getUserDTO(User user) {
+        var userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setUsername(user.getUsername());
+        var userDetail = user.getUserDetail();
+        var address = user.getAddress();
+        if (userDetail != null) {
+            userDTO.setPhone(userDetail.getPhone());
+            var nameDTO = new NameDTO();
+            nameDTO.setFirstname(userDetail.getFirstname());
+            nameDTO.setLastname(userDetail.getLastname());
+            userDTO.setName(nameDTO);
+        }
+        if (address != null) {
+            var addressDTO = new AddressDTO();
+            addressDTO.setCity(address.getCity());
+            addressDTO.setStreet(address.getStreet());
+            addressDTO.setNumber(address.getNumber());
+            addressDTO.setZipcode(address.getZipcode());
+            var geolocation = address.getLocation();
+            if (geolocation != null) {
+                var geolocationDTO = new GeolocationDTO();
+                addressDTO.setGeolocation(geolocationDTO);
+            }
+            userDTO.setAddress(addressDTO);
+        }
+        return userDTO;
+    }
+
+    private Pageable getPageable(Optional<Integer> optionalLimit, Optional<String> optionalSort) {
+        var limit = optionalLimit.isPresent() ? optionalLimit.get() : (int) this.userRepository.count();
+        var direction = optionalSort.isPresent() ? optionalSort.get().equals("asc") ? Direction.ASC : Direction.DESC
+                : Direction.ASC;
+        return PageRequest.of(0, limit, direction, "id");
+    }
+}
